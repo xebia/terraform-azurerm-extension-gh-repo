206b67f3388fcaa449e4c8d582c07f4af8dbfb68
patryk.gasiorowski <patryk.gasiorowski@xebia.com>
2025-10-22 16:17:17 +0200

    Update provider versions in Terraform configuration

--- src/versions.tf
+++ src/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.116.0"
+      version = "~> 4.14.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.51.0"
+      version = "~> 3.0.2"
     }
     github = {
       source  = "integrations/github"
