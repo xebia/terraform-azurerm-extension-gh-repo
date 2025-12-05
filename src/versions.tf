0ab0b9f6a6bdcab6ccf3ba9a59879cd19560738d
Marcin Słowikowski <59068696+msl0@users.noreply.github.com>
2025-12-05 14:12:22 +0100

    Updated provider's version constraint

--- src/versions.tf
+++ src/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.14.0"
+      version = "~> 4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3.0.2"
+      version = "~> 3.0"
     }
     github = {
       source  = "integrations/github"
